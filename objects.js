44ce6455e621589dad448679dcb1a8a7b6481cba
Muhammad Umair Khan <129543803+umairkhanzz@users.noreply.github.com>
2024-03-26 23:04:55 +0500

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// change the month and year
+var month = 2, year = 2022;
+var day = 0;
+switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        day = 31;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        day = 30;
+        break;
+    case 2:
+        // leap year
+        if (((year % 4 == 0) &&
+            !(year % 100 == 0))
+            || (year % 400 == 0))
+            day = 29;
+        else
+            day = 28;
+        break;
+    default:
+        throw Error('Invalid month');
+}
+console.log("The month ".concat(month, " in ").concat(year, " has ").concat(day, " days"));
